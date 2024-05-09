bda665ad933c28d563534e7b2d7ff9a5c3db611d
pamzak <pamirzaki2014@gmail.com>
2024-05-08 21:21:42 -0700

    add first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:parallax_slide_animation/tabs.dart';
+
+import 'exhibition_bottom_sheet.dart';
+import 'sliding_cards.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(), //use this or ScrollableExhibitionSheet
+        ],
+      ),
+    );
+  }
+}
